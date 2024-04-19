6c0a15f90512ce7e7de18046783446b803198f74
ABID066 <mdabid1152@gmail.com>
2024-04-19 20:48:31 +0600

    module 16 Running

---
+++ module 16/vite-project1/src/App.jsx
@@ -0,0 +1,56 @@
+import { useState } from "react"
+
+
+
+function App() {
+
+
+  const [number, setNumber]=useState(1)
+
+  /*const changeNumber=()=>{
+
+    setNumber(number+1)
+  }*/
+
+  const  [myObj,setObj]=useState({
+      key1:"value 1",
+      key2:"value 2",
+      key3:"value 3"
+  })
+  const changeTheValueOfObj=()=>{
+     /* setObj({
+        key1:"new Value1",
+        key2:"new Value2",
+        key3:"new Value3",
+        key4:"new Value4",   //Change the hole object
+        key5:"new Value5",
+      })*/
+      setObj(
+        pObj=>({...pObj,
+          key1:"NEW VALUE FOR key 1" 
+          //change only specific value of keys
+        })
+      )
+  }
+
+
+
+  return (
+    <div>
+      <h1>Number: {number}</h1>
+      <button onClick={()=>setNumber(number+1)}><h3>Click</h3></button>
+      <br/><br/><br/><br/>
+
+
+      <h1>{myObj.key1}</h1>
+      <h1>{myObj.key2}</h1>
+      <h1>{myObj.key3}</h1>
+      <h1>{myObj.key4}</h1>
+      <h1>{myObj.key5}</h1>
+      <button onClick={changeTheValueOfObj}>Change</button>
+
+    </div>
+  )
+}
+
+export default App
--- module 16/vite-project1/src/main.jsx
+++ module 16/vite-project1/src/main.jsx
@@ -1,8 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from "./App.jsx"
-import 'bootstrap'
-import 'bootstrap/dist/css/bootstrap.css'
+import App from './App.jsx'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
